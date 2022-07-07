18e62bb946bfb365e29f6965d41393399b991231
unknown <laralourenco5966@gmail.com>
2022-07-07 16:32:08 -0300

    remoção do react  icons para teste

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import './App.css';
 import { useState, useEffect } from 'react';
-import { BsTrash, BsBookmarkCheck, BsBookmarkCheckFill } from 'react-icons';
 
 const API = "http://localhost:5000";
 
